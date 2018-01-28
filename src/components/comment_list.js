2ecde65168c7af0149e1369cdf4d759e316e8b2d
Joe Cowton <joecowton@gmail.com>
2018-01-28 18:05:03 +0000

    testing comments list

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux'
+
 
 const CommentList = (props) => {
+  const list = props.comments.map(comment => <li key={comment}>{comment}</li>)
+
   return (
-    <ul className="comment-list">Hello</ul>
+    <ul className="comment-list">{list}</ul>
   )
 }
 
+function mapStateToProps(state) {
+  return { comments: state.comments };
+}
 
-export default CommentList;
+
+export default connect(mapStateToProps)(CommentList);
