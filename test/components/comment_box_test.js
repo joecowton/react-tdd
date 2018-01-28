2314541eda5fa5fd4aa442dd8b2841dd7d918277
Joe Cowton <joecowton@gmail.com>
2018-01-28 17:30:53 +0000

    comment specs

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -10,7 +10,7 @@ describe('CommentBox', () => {
   })
 
   it('has the correct class', () =>{
-    expect(component).to.have.class('comment-box')
+    expect(component).to.have.class('comment-box');
   })
 
   it('has a text area', () => {
@@ -20,4 +20,19 @@ describe('CommentBox', () => {
   it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    })
+
+
+    it('shows text in the text area', () => {
+      expect(component.find('textarea')).to.value('new comment')
+    })
+
+    it('when submitted, clears the input', () => {
+      
+    })
+  })
 });
